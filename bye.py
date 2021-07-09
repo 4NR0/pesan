c45cf5035c0a1ee574b976e8ac52f7a5e113f235
4NR0 <87198134+4NR0@users.noreply.github.com>
2021-07-09 22:50:52 +0700

    Add files via upload

---
+++ bye.py
@@ -0,0 +1,21 @@
+import sys, time
+def jalan(kata):
+	for e in kata:
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.1)
+jalan(' Hai kamu, iya kamu, aku cuma mau bilang'.center(25))
+
+jalan(' Aku mau pamit, Terimakasih ya kamu udah'.center(25))
+
+jalan('  mau nemenin aku chat:) aku pamit'.center(25))
+
+jalan(' Oh iya 1 lagi, aku suka sama kamu. I LOVE YOU'.center(25))
+
+jalan(' and bye bye:) Semoga kamu bahagia, Aku nunggu'.center(25))
+
+jalan(' kamu disini kock:)Walapun dah beda'.center(25))
+
+jalan(' alam, aku tetap suka sama kamu. I LOVE YOU'.center(25))
+
+#GUA BUAT INI AGAK SEDIH ANJIR WKWK
